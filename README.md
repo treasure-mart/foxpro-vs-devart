fa68282c2b714f4cc16a458f6098613d35827554
Ryan Anstey <mail@ryfi.io>
2019-10-29 12:16:13 -0700

    Update for BETWEEN issue

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-I can't run UPDATE queries to the ARCUST01 database.
+BETWEEN syntax not working.
 
-- The code is demonstrated in the `index.php` file.
-- It outputs to `results.txt`.
-- The database in question is under the `/db` folder.
-
-The problem lies with the connection settings. We can't run updates to this table with "Native" IndexOnReading, but most other tables won't let us use "Local".
-
-- "VisualFoxPro" and "Native" doesn't work.
-- "VisualFoxPro" and "Local" works.
-- "FoxPro2" and "Native" doesn't work.
-- "FoxPro2" and "Local" works.
-
-The reverse of this issue is the problem for all of our other databases. All of them have their own indexes so we need to use "Native" to update them properly. "Local" never works, except in this one case.
+- index.php contains my test code.
+- Test databases are under `/db`.
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+//----------------------------------------
+// Connection
+//----------------------------------------
+
+$foxproConnection = odbc_connect("Driver={Devart ODBC Driver for xBase};Database=/srv/www/sbtdev/root/db;IdentifierCase=icLower;DBF Format=VisualFoxPro;IndexOnReading=Native;", '', '');
+$file = '/srv/www/sbtdev/root/results.txt';
+
+if (!$foxproConnection) {
+    echo odbc_error($foxproConnection);
+    die("Connection to foxpro could not be made.");
+}
+
+//----------------------------------------
+// SELECT query
+//----------------------------------------
+
+$query = "SELECT custno, entered FROM ARCUST01;";
+
+$results = odbc_exec($foxproConnection, $query) or die ("<h2>Error message</h2><pre>".odbc_errormsg($foxproConnection)."</pre>"."<h2>Query that attempted to run</h2><pre>".$query."</pre>");
+
+while($row=odbc_fetch_array($results)) {
+    $output1 .= $row['custno'].' was created on "'.$row['entered'].'"'.PHP_EOL;
+    $output1 .= PHP_EOL;
+}
+
+//----------------------------------------
+// OUTPUT
+//----------------------------------------
+
+echo '<h2>Query that returns all records from database:</h2>';
+
+echo "<pre>".$query."</pre>";
+
+echo '<h2>Results:</h2>';
+
+echo "<pre>".$output1."</pre>";
+
+//----------------------------------------
+// SELECT query 2
+//----------------------------------------
+
+$query = "SELECT custno, entered FROM ARCUST01 WHERE entered BETWEEN 2000-01-01 AND 2015-01-01;";
+//$query = "SELECT custno, entered FROM ARCUST01 WHERE entered BETWEEN 01/01/2000 AND 01/01/2015;";
+
+$results = odbc_exec($foxproConnection, $query) or die ("<h2>Error message</h2><pre>".odbc_errormsg($foxproConnection)."</pre>"."<h2>Query that attempted to run</h2><pre>".$query."</pre>");
+
+while($row=odbc_fetch_array($results)) {
+    $output2 .= $row['custno'].' was created on "'.$row['entered'].'"'.PHP_EOL;
+    $output2 .= PHP_EOL;
+}
+
+//----------------------------------------
+// OUTPUT
+//----------------------------------------
+
+echo '<h2>Query that returns records filtered between 2000 and 2015:</h2>';
+
+echo "<pre>".$query."</pre>";
+
+echo '<h2>Results:</h2>';
+
+echo "<pre>".$output2."</pre>";
+
+file_put_contents($file, $output1.$output2);
+
+echo 'foxpro finished!<br>';
+?>

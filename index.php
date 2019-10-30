43ab1af16169fc006d3b04bf462375f181878b7d
Ryan Anstey <mail@ryfi.io>
2019-10-30 08:46:44 -0700

    Adjust syntax to Devart specs

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ echo "<pre>".$output1."</pre>";
 // SELECT query 2
 //----------------------------------------
 
-$query = "SELECT custno, entered FROM ARCUST01 WHERE entered BETWEEN 2000-01-01 AND 2015-01-01;";
+$query = "SELECT custno, entered FROM ARCUST01 WHERE entered BETWEEN '2000-01-01' AND '2015-01-01';";
 //$query = "SELECT custno, entered FROM ARCUST01 WHERE entered BETWEEN 01/01/2000 AND 01/01/2015;";
 
 $results = odbc_exec($foxproConnection, $query) or die ("<h2>Error message</h2><pre>".odbc_errormsg($foxproConnection)."</pre>"."<h2>Query that attempted to run</h2><pre>".$query."</pre>");
